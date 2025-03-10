41b4018262b3415532ab5449cd7248c48b225dff
Azkal182 <mohazkalarif@gmail.com>
2025-03-10 07:07:22 +0700

    add perngajuan

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,28 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
+// let prisma: PrismaClient
 
-declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined
-}
+// declare global {
+//     // eslint-disable-next-line no-var
+//     var prisma: PrismaClient | undefined
+// }
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient()
+// } else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient()
+//     }
+//     prisma = global.prisma
+// }
+
+// export default prisma
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma
