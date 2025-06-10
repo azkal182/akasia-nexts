c7e854ab809e24920e965b1a5d5692bda7c42080
Azkal182 <mohazkalarif@gmail.com>
2025-06-11 00:32:36 +0700

    fix middleware register

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true
+      }
+    ];
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '25mb'
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const page = () => {
-  // redirect to dashboard
-  if (typeof window !== 'undefined') {
-    window.location.href = '/dashboard';
-  }
-  return <div>...</div>;
-};
